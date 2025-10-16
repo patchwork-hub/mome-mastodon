441928f791cb60c66967cd9b3ea6301bc1e7c19c
NangKhatMang <50686926+NangKhatMang@users.noreply.github.com>
2025-10-16 15:55:58 +0630

    Merge Pull request from development to production (#12)
    * update: accounts gem
    
    * update: mome logo change

--- app/javascript/mastodon/components/logo.tsx
+++ app/javascript/mastodon/components/logo.tsx
@@ -1,19 +1,13 @@
-import logo from '@/images/logo.svg';
+import logo from '@/images/mo-me-logo.png';
 
 export const WordmarkLogo: React.FC = () => (
-  <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
-    <title>Mastodon</title>
-    <use xlinkHref='#logo-symbol-wordmark' />
-  </svg>
+  <img src={logo} alt='Mo-Me' className='logo logo--wordmark' />
 );
 
 export const IconLogo: React.FC = () => (
-  <svg viewBox='0 0 79 79' className='logo logo--icon' role='img'>
-    <title>Mastodon</title>
-    <use xlinkHref='#logo-symbol-icon' />
-  </svg>
+  <img src={logo} alt='Mo-Me' className='logo logo--icon' />
 );
 
 export const SymbolLogo: React.FC = () => (
-  <img src={logo} alt='Mastodon' className='logo logo--icon' />
+  <img src={logo} alt='Mo-Me' className='logo logo--icon' />
 );
